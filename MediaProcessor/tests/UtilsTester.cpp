119951d7d2882d29a2d0b3d3e145515380d2c9cb
Nikhil <nikhiljangra264@gmail.com>
2025-01-01 20:30:01 +0530

    Testing: Add new tests for `Utils`
    1. Add `runCommand_Success`, `runCommand_Fail`, `getMediaDuration_validFile_Success`, `getMediaDuration_invalidFile_Fail` tests.
    
    2. Rename `trimTrailingSpaces` to `trimTrailingSpaces_Success`.

--- MediaProcessor/tests/UtilsTester.cpp
+++ MediaProcessor/tests/UtilsTester.cpp
@@ -30,12 +30,42 @@ TEST(UtilsTester, EnsureDirectoryExists) {
     fs::remove_all(tempPath);
 }
 
-TEST(UtilsTester, TrimTrailingSpaces) {
+TEST(UtilsTester, trimTrailingSpaces_Success) {
     std::string inputWithTrailingSpace = "Hello, World! ";
     std::string inputWithoutTrailingSpace = "Hello, World!";
 
     EXPECT_EQ(inputWithoutTrailingSpace, Utils::trimTrailingSpace(inputWithTrailingSpace));
     EXPECT_EQ(inputWithoutTrailingSpace, Utils::trimTrailingSpace(inputWithoutTrailingSpace));
+}
+
+TEST(UtilsTester, runCommand_Success) {
+    std::string command = "echo \"Hello\"";
+    EXPECT_TRUE(Utils::runCommand(command));
+
+    auto output = Utils::runCommand(command, true);
+    EXPECT_TRUE(output.has_value());
+    EXPECT_EQ(*output, "Hello\n");
+}
+
+TEST(UtilsTester, runCommand_Fail) {
+    std::string invalid_command = "invalid_command\nanything/@&h";
+    EXPECT_FALSE(Utils::runCommand(invalid_command));
+
+    auto output = Utils::runCommand(invalid_command, true);
+    EXPECT_FALSE(output.has_value());
+}
+
+TEST(UtilsTester, getMediaDuration_validFile_Success) {
+    fs::path testMediaPath(TEST_MEDIA_DIR);
+    testMediaPath.make_preferred();
+    fs::path audioPath = testMediaPath / "test_audio_processed.wav";
+    // precomupted length of test_audio_processed.wav
+    EXPECT_EQ(Utils::getMediaDuration(audioPath), 1.9635);
+}
+
+TEST(UtilsTester, getMediaDuration_invalidFile_Fail) {
+    fs::path invalidAudioPath("This path doesn't exist");
+    EXPECT_EQ(Utils::getMediaDuration(invalidAudioPath), -1);
 }
 
 }  // namespace MediaProcessor::Tests
